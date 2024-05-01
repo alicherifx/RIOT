d7434ad21f92257ddd63f2c7b754c29824cf66b3
Islem Azouz <isleem.azouuz@gmail.com>
2024-05-01 22:47:45 +0200

    stream

--- src/components/home/login.js
+++ src/components/home/login.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Form, Input, Button } from 'antd';
-import { CaretUpOutlined, CaretDownOutlined, CaretLeftOutlined, CaretRightOutlined } from '@ant-design/icons';
+import { CaretUpOutlined, CaretDownOutlined, CaretLeftOutlined, CaretRightOutlined,} from '@ant-design/icons';
 import vid from '../../assets/videos/silvergoku.mp4'
 
 function AppLogin({ loggedIn, onLogin }) {
@@ -10,36 +10,39 @@ function AppLogin({ loggedIn, onLogin }) {
 
   if (loggedIn) {
     return (
-      <div id="robot-control" className="block">
-        <div className="container-fluid">
+      <div id="robot-control" className="block" style={{ height: '100%' }}>
+        <div className="container-fluid" style={{ height: '100%' }}>
           <div className="titleHolder">
             <h2>Robot Control</h2>
           </div>
-          <div className="video-and-controls">
-
-            <div className="video-container">
-            <h1>Live Streaming</h1>
-              <video controls width="600" height="400">
+          <div className="video-and-controls" style={{ display: 'flex', height: '100%' }}>
+            <div className="control-buttons" style={{ flex: 1 }}>
+              <div style={{ margin: '8px' }}>
+                <Button type="primary" icon={<CaretUpOutlined />} />
+              </div>
+              <div style={{ padding: '8px' }}>
+                <Button type="primary" icon={<CaretLeftOutlined />} style={{ marginRight: '8px' }} />
+                <Button type="primary">Stop</Button>
+                <Button type="primary" icon={<CaretRightOutlined />} style={{ marginLeft: '8px' }} />
+              </div>
+              <div style={{ marginTop: '8px' }}>
+                <Button type="primary" icon={<CaretDownOutlined />} />
+              </div>
+            </div>
+            <div className="video-container" style={{ flex: 1, height: '100%' }}>
+              <h1>Live Streaming</h1>
+              <video controls width="100%" height="100%">
                 <source src={vid} type="video/mp4" />
                 Your browser does not support the video tag.
               </video>
             </div>
-            <div className="control-buttons">
-            <h1>Robot commands</h1>
-          <div>
-              <Button type="primary" icon={<CaretUpOutlined />} />
-           </div>
-          <div>
-            <Button type="primary" icon={<CaretLeftOutlined />} />
-            <Button type="primary" icon={<CaretDownOutlined />} />
-            <Button type="primary" icon={<CaretRightOutlined />} />
+            <div className="divider" style={{ width: '2px', height: '100%', background: '#ccc' }}></div>
           </div>
-           </div>
-      </div>
         </div>
       </div>
     );
   }
+
 
   return (
     <div id="login" className="block loginBlock">
